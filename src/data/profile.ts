2659061473cc67e7e8a37f8d3d6d89a2569c9fe6
Alan Ortiz <alanortiz@MacBook-Air-de-personal-de-Alan.local>
2025-04-10 15:46:07 +0200

    Cambios en logos e imagenes

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -3,7 +3,8 @@ const profile = {
     profession: "Desarrollador Fullstack",
     address: "Valencia, España",
     email: "alan.omar.ortz@gmail.com",
-    avatar: "/images/alan-profile.png",
+    avatar: "/images/alan-profile.webp",
+    aboutMeLogo: "/images/alan-about.webp",
     aboutMe: `Hola, me llamo Alan Ortiz. Soy desarrollador fullstack con más de 6 años de experiencia construyendo soluciones a la medida para empresas en sectores como <strong>eCommerce, fintech y comunicación digital</strong>.\n\n` +
     `Me especializo en el stack <strong>JavaScript (React, Node, TypeScript)</strong>, y también he trabajado con <strong>PHP, Java y sistemas basados en VTEX</strong>.\n\n` +
     `Durante mi carrera he colaborado en equipos de distintos tamaños, desde startups hasta corporativos, siempre adaptándome a contextos técnicos diversos y formas de trabajo dinámicas.\n\n` +
